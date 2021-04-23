f600283d912f63faf47861669063e0ee483aa418
dalvara6 <77400514+dalvara6@users.noreply.github.com>
2021-04-23 12:10:08 -0400

    Update SkinCleanser.py

--- SkinCleanser.py
+++ SkinCleanser.py
@@ -1,3 +1,7 @@
-#first we will be asking the consumer what kind of skin they have and if they put unknown we will describe the skin types in detail
-input("Your skin is one of the most important things to take care of, the purpose of this program is help you find the best cleanser based on your skin type! Please enter your skin type(dry, oily, combo, or unknown):")
-
+#first we will be giving the consumer a basic understanding of what this program will do
+print("Thank you for choosing Skin Cleanser Finder to figure out the best skin care for you! We will start off with giving some basic insight on the different skin types.")
+#now we are going to start by giving them information
+print("There's 3 main types of skin, oily, dry and combonation. To start off we will explain and then from there you could get an idea and hopefully that will help you find which sounds like you. Dry skin type is usually described and dehydrated, flaky and cracked. Oily skin is usually described as greasy, shiny, and clogged pores.Finally combonation skin is usually described as a little bit of both, meaning you could be dry around your chin and oily in your t-zone.")
+#now I will ask the consumer for their input to start getting an idea
+input("After reading through the top what kind of skin do you think you lean towards?(dry, oily, combo): ")
+#next we will 
